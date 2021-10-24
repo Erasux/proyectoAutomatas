2a4c449bc02a39c4fd96b65c2bdf1037f31ec700
camian0 <camian6@outlook.com>
2021-10-23 22:09:37 -0500

    agregamos método para leer archivo JSON y que devuelva un array con los automatas cargados

--- src/Modelo/JSON.java
+++ src/Modelo/JSON.java
@@ -4,29 +4,42 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Map;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import Modelo.Automata;
+import java.util.ArrayList;
 
 //class for read json files
 public class JSON {
 
-    public static void main(String[] args) {
+    ArrayList<Automata> automatas = new ArrayList<Automata>();
+
+    //método para leer archivos json 
+    public void readJSONFile() {
         JSONParser jsonParser = new JSONParser();
         try {
-            Object obj = jsonParser.parse(new FileReader("\\automatas.json"));
-            JSONObject jsonObject = (JSONObject) obj;
+            Object jsonFile = jsonParser.parse(new FileReader("..\\proyectoAutomatas\\automatas.json"));
+            JSONObject jsonObjects = (JSONObject) jsonFile;
+            Iterator entries = jsonObjects.entrySet().iterator();
+            String automata = "";
+            Object valor = null;
+            while (entries.hasNext()) {
+                Map.Entry entry = (Map.Entry) entries.next();
+                automata = (String) entry.getKey();
+                valor = (Object) entry.getValue();
+            }
 
             String hola = "";
 
         } catch (FileNotFoundException e) {
-            //manejo de error
+            System.out.println(e);
         } catch (IOException e) {
-            //manejo de error
+            System.out.println(e);
         } catch (ParseException e) {
-            //manejo de error
+            System.out.println(e);
         }
     }
-
 }
